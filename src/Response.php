e3993cdd03dbddcb59e17de4cb5c308244eb59bb
huanjiesm <huanjiesm@163.com>
2019-09-29 23:54:47 +0800

    添加response的基础方法

--- src/Response.php
+++ src/Response.php
@@ -18,4 +18,52 @@ class Response{
         $this->res->end($item);
     }
 
+    public function status($code){
+
+    }
+
+    public function header($key,$value){
+
+    }
+
+    /**
+     * 设置Cookie
+     * 
+     * @param string $opts.name 
+     * @param string $opts.value
+     * @param int $opts.maxage
+     * @param string $opts.path
+     * @param string $opts.domain
+     * @param bool $opts.secure
+     * @param bool $opts.httponly
+     */
+    public function cookie($opts){
+
+    }
+
+    public function html($value,$code=200){
+
+    }
+
+    public function json($value,$code=200){
+
+    }
+
+    public function xml($value,$code=200){
+
+    }
+
+    public function string($value,$code=200){
+
+    }
+
+
+    public function redirect(){
+
+    }
+
+    public function file($filepath){
+
+    }
+
 }
