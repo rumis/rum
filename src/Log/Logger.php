c6bf1feefb47faa749b682748ade90f2d416f8d3
liumurong1 <liumurong1@100tal.com>
2019-10-11 21:44:50 +0800

    实现console日志

--- src/Log/Logger.php
+++ src/Log/Logger.php
@@ -1,14 +1,144 @@
 <?php
 
-namespace Rum;
+namespace Rum\Log;
+
+use Psr\Log\LoggerInterface;
 
 /**
  * 
  */
-class Logger{
+class Logger
+{
+    static $logger;
 
-    static function log(){
-
+    /**
+     * Sets a logger instance on the object.
+     *
+     * @param LoggerInterface $logger
+     * @return void
+     */
+    public static function setLogger(LoggerInterface $logger)
+    {
+        self::$logger = $logger;
     }
-    
+
+    /**
+     * System is unusable.
+     *
+     * @param string $message
+     * @param array $context
+     * @return void
+     */
+    public static function emergency($message, array $context = array())
+    {
+        self::$logger->emergency($message, $context);
+    }
+
+    /**
+     * Action must be taken immediately.
+     *
+     * Example: Entire website down, database unavailable, etc. This should
+     * trigger the SMS alerts and wake you up.
+     *
+     * @param string $message
+     * @param array $context
+     * @return void
+     */
+    public static function alert($message, array $context = array())
+    {
+        self::$logger->alert($message, $context);
+    }
+
+    /**
+     * Critical conditions.
+     *
+     * Example: Application component unavailable, unexpected exception.
+     *
+     * @param string $message
+     * @param array $context
+     * @return void
+     */
+    public static function critical($message, array $context = array())
+    {
+        self::$logger->critical($message, $context);
+    }
+
+    /**
+     * Runtime errors that do not require immediate action but should typically
+     * be logged and monitored.
+     *
+     * @param string $message
+     * @param array $context
+     * @return void
+     */
+    public static function error($message, array $context = array())
+    {
+        self::$logger->error($message, $context);
+    }
+
+    /**
+     * Exceptional occurrences that are not errors.
+     *
+     * Example: Use of deprecated APIs, poor use of an API, undesirable things
+     * that are not necessarily wrong.
+     *
+     * @param string $message
+     * @param array $context
+     * @return void
+     */
+    public static function warning($message, array $context = array())
+    {
+        self::$logger->warning($message, $context);
+    }
+
+    /**
+     * Normal but significant events.
+     *
+     * @param string $message
+     * @param array $context
+     * @return void
+     */
+    public static function notice($message, array $context = array())
+    {
+        self::$logger->notice($message, $context);
+    }
+
+    /**
+     * Interesting events.
+     *
+     * Example: User logs in, SQL logs.
+     *
+     * @param string $message
+     * @param array $context
+     * @return void
+     */
+    public static function info($message, array $context = array())
+    {
+        self::$logger->info($message, $context);
+    }
+
+    /**
+     * Detailed debug information.
+     *
+     * @param string $message
+     * @param array $context
+     * @return void
+     */
+    public static function debug($message, array $context = array())
+    {
+        self::$logger->debug($message, $context);
+    }
+
+    /**
+     * Logs with an arbitrary level.
+     *
+     * @param mixed $level
+     * @param string $message
+     * @param array $context
+     * @return void
+     */
+    public static function log($level, $message, array $context = array())
+    {
+        self::$logger->log($message, $context);
+    }
 }
